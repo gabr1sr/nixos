2c262e19d66a13d5c8313bb475d38b236bc3dbad
Gabriel Rosa <gabr1sr@proton.me>
2023-12-19 16:56:19 -0300

    add: flakes + home-manager configuration

---
+++ build.sh
@@ -0,0 +1,2 @@
+sudo nixos-rebuild switch --flake '.#desktop'
+home-manager switch --flake '.#gabr1sr@desktop'
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hardware.url = "github:NixOS/nixos-hardware";
+    nur.url = "github:nix-community/nur";
+
+    homeManager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nur, hardware, ... }@inputs: {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+	      modules = [
+	        ./host/desktop/configuration.nix
+	      ];
+      };
+    };
+
+    homeConfigurations = {
+      "gabr1sr@desktop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	      extraSpecialArgs = { inherit inputs; };
+	      modules = [
+	        ./home/desktop/gabr1sr/default.nix
+
+          {
+	          imports = [ inputs.nur.hmModules.nur ];
+	        }
+	      ];
+      };
+    };
+  };
+}
