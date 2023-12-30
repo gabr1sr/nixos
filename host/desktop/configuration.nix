12a36914d9640b89d04decb4662a0f6b77912405
Gabriel Rosa <gabr1sr@proton.me>
2023-12-30 17:09:49 -0300

    fix: firejail wrappedBinaries

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -146,8 +146,15 @@
   programs.firejail = {
     enable = true;
     wrappedBinaries = {
-      executable = "${pkgs.firefox}/bin/firefox";
-      profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+      firefox = {
+        executable = "${pkgs.firefox}/bin/firefox";
+        profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+      };
+
+      discord = {
+        executable = "${pkgs.discord}/bin/discord";
+        profile = "${pkgs.firejail}/etc/firejail/discord.profile";
+      };
     };
   };
 
