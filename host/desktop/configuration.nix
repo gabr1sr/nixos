1416f4802827f38685aa03fe533fb953a32cd86a
Gabriel Rosa <gabr1sr@proton.me>
2024-01-22 22:23:52 -0300

    add: mangohud package

--- host/desktop/configuration.nix
+++ host/desktop/configuration.nix
@@ -41,18 +41,18 @@
 
   time.timeZone = "America/Sao_Paulo";
 
-  i18n.defaultLocale = "pt_BR.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   services.xserver.enable = true;
@@ -114,6 +114,7 @@
     hunspell
     hunspellDicts.pt_BR
     tor-browser
+    mangohud
 
     (steam.override {
       extraPkgs = pkgs: [
